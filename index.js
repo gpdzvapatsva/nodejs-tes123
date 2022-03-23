0b8ade8380fbc00fee40297ad0d56a30787abd5b
gpdzvapatsva <gpdzvapatsva@gmail.com>
2022-03-23 14:16:43 +0200

    hfgfgf

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ app.post("/api/courses/", (req, res) => {
   courses.push(course);
   res.send(course);
 });
-const port = 3000;
-//const port =process.env.PORT || 3000
+//const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening to ${port}`);
 });
